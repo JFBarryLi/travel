c35dd0c78ac325442040d3cd6cceceb7e9ad1cda
Barry <bli031@uottawa.ca>
2022-03-27 14:09:07 +0000

    Added parse_locality.

--- pipeline/notes/disect.py
+++ pipeline/notes/disect.py
@@ -6,8 +6,9 @@ log = logging.getLogger(__name__)
 
 def parse(txt):
     day = parse_day(txt)
-    from_locality = None
-    to_locality = None
+    locality = parse_locality(txt)
+    from_locality = locality['from_locality']
+    to_locality = locality['to_locality']
     date = None
     body = None
 
@@ -28,3 +29,26 @@ def parse_day(txt):
         return int(day)
     except Exception as e:
         log.error(f'Failed to parse day from text. Exception: {e}')
+
+
+def parse_locality(txt):
+    try:
+        line = txt.split('\n')[1].split('-')
+
+        # Handle cases where '-' is in the locality's name
+        if len(line) > 2:
+            locality = '-'.join(line[:-1]).strip()
+        else:
+            locality = line[0].strip()
+
+        if '/' in locality:
+            loc_split = locality.split('/')
+            from_locality = loc_split[0]
+            to_locality = loc_split[1]
+        else:
+            from_locality = locality
+            to_locality = locality
+
+        return {'from_locality': from_locality, 'to_locality': to_locality}
+    except Exception as e:
+        log.error(f'Failed to parse locality from text. Exception: {e}')
--- pipeline/notes/tests/test_disect.py
+++ pipeline/notes/tests/test_disect.py
@@ -1,5 +1,5 @@
 from . import examples
-from ..disect import parse, parse_day
+from ..disect import parse, parse_day, parse_locality
 
 
 def test_parse():
@@ -11,3 +11,45 @@ def test_parse_day():
         day = int(exp_name.split('_')[0])
         disected = parse_day(examples[exp_name])
         assert disected == day
+
+
+def test_parse_locality_one_no_space():
+    exp = examples['3_std.txt']
+    locality = parse_locality(exp)
+    assert locality['from_locality'] == 'Seattle'
+    assert locality['to_locality'] == 'Seattle'
+
+
+def test_parse_locality_with_space():
+    exp = examples['1_std.txt']
+    locality = parse_locality(exp)
+    assert locality['from_locality'] == 'Seattle'
+    assert locality['to_locality'] == 'San Francisco'
+
+
+def test_parse_locality_one_with_space():
+    exp = examples['2_std.txt']
+    locality = parse_locality(exp)
+    assert locality['from_locality'] == 'San Francisco'
+    assert locality['to_locality'] == 'San Francisco'
+
+
+def test_parse_locality_bad_spacing_1():
+    exp = examples['7_bad_spacing.txt']
+    locality = parse_locality(exp)
+    assert locality['from_locality'] == 'Los Angeles'
+    assert locality['to_locality'] == 'San Jose'
+
+
+def test_parse_locality_bad_spacing_2():
+    exp = examples['8_bad_spacing.txt']
+    locality = parse_locality(exp)
+    assert locality['from_locality'] == 'San Jose'
+    assert locality['to_locality'] == 'San Jose'
+
+
+def test_parse_locality_dash_in_locality():
+    exp = examples['13_dash_in_locality.txt']
+    locality = parse_locality(exp)
+    assert locality['from_locality'] == 'San Jose'
+    assert locality['to_locality'] == 'Ivano-Frankivsk'
