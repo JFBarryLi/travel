b8f4d7e9999e23aab8387fc7ae4872e04139911f
Barry <bli031@uottawa.ca>
2022-04-11 13:51:58 +0000

    Added lru cache to geo_code.

--- pipeline/notes/geo_code.py
+++ pipeline/notes/geo_code.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 
 from geopy.geocoders import Nominatim
@@ -6,9 +7,25 @@ from geopy.extra.rate_limiter import RateLimiter
 log = logging.getLogger(__name__)
 
 geolocator = Nominatim(user_agent="barry_li_notes_processing_pipeline")
+geocode_limit = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+geocode = functools.lru_cache(maxsize=1024)(
+    functools.partial(geocode_limit, timeout=5)
+)
 
 
 def geo_code(location):
-    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
     loc = geocode(location)
-    return loc
+    try:
+        log.info(f'Geocoding: {location}.')
+        return {
+            'address': loc.address,
+            'latitude': loc.latitude,
+            'longitude': loc.longitude,
+        }
+    except Exception as e:
+        log.error(f'Failed to geocode: {location}. Exception: {e}.')
+        return {
+            'address': None,
+            'latitude': None,
+            'longitude': None,
+        }
