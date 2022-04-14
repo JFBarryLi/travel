abf7e01495dd42668ce894eb5951e8ceb7a7a666
Barry <bli031@uottawa.ca>
2022-04-14 13:29:08 +0000

    Added DiscordHandler.

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,8 +1,26 @@
+import os
 import logging
 from rich.logging import RichHandler
+from .utils.discord_handler import DiscordHandler
+
+discord_webhook = os.environ['ALERT_WEBHOOK']
+
+format = logging.Formatter('$(levelname)s | %(name)s | %(message)s')
 
 logging.basicConfig(
     format='%(levelname)s | %(name)s | %(message)s',
     level=logging.INFO,
     handlers=[RichHandler(rich_tracebacks=True)],
+)
+
+discord_handler = DiscordHandler(discord_webhook, 'notes-pipeline-alert-agent')
+discord_handler.setFormatter(format)
+discord_handler.setLevel(logging.INFO)
+
+logging.basicConfig(
+    format='%(levelname)s | %(name)s | %(message)s',
+    level=logging.INFO,
+    handlers=[
+        RichHandler(rich_tracebacks=True),
+        discord_handler],
 )
---
+++ pipeline/utils/discord_handler.py
@@ -0,0 +1,58 @@
+import time
+import logging
+import requests
+
+
+class DiscordHandler(logging.Handler):
+    def __init__(self, webhook_url, agent):
+        logging.Handler.__init__(self)
+        self.webhook_url = webhook_url
+        self.retry_limit = 3
+        self.retry_count = 0
+        self.x_ratelimit_remaining = None
+        self.x_ratelimit_reset_after = None
+        self.headers = {'User-Agent': agent}
+
+    def write_to_discord(self, message):
+        if self.x_ratelimit_remaining is None:
+            pass
+        elif self.x_ratelimit_remaining < 1:
+            if self.x_ratelimit_reset_after is not None:
+                time.sleep(self.x_ratelimit_reset_after)
+            else:
+                time.sleep(2)
+
+        request = requests.post(
+            self.webhook_url,
+            headers=self.headers,
+            data={
+                'content': message
+            }
+        )
+
+        self.x_ratelimit_reset_after = int(request.headers['x-ratelimit-reset-after'])
+        self.x_ratelimit_remaining = int(request.headers['x-ratelimit-remaining'])
+
+        if request.status_code == 404:
+            raise requests.exceptions.InvalidURL(f'Discord Webhook 404: {request.text}')
+
+        if request.status_code == 429:
+            if self.retry_count < self.retry_limit:
+                if self.x_ratelimit_reset_after is not None:
+                    time.sleep(self.x_ratelimit_reset_after)
+                else:
+                    time.sleep(2)
+                self.retry_count += 1
+                self.write_to_discord
+
+        if not request.ok:
+            raise requests.exceptions.HTTPError(
+                f'Discord WebHook returned status code {request.status_code}, {request.text}'
+            )
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            self.write_to_discord(f'```{msg}```')
+        except Exception:
+            self.handleError(record)
