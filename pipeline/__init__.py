4b7084c6a02de8931e218a6a6071a7b47a9f9561
barry@barryli.ca <barry@barryli.ca>
2022-09-11 21:48:36 +0100

    Fixed dicord logging handler.

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -5,13 +5,7 @@ from .utils.discord_handler import DiscordHandler
 
 discord_webhook = os.environ['ALERT_WEBHOOK']
 
-format = logging.Formatter('$(levelname)s | %(name)s | %(message)s')
-
-logging.basicConfig(
-    format='%(levelname)s | %(name)s | %(message)s',
-    level=logging.INFO,
-    handlers=[RichHandler(rich_tracebacks=True)],
-)
+format = logging.Formatter('%(levelname)s | %(name)s | %(message)s')
 
 discord_handler = DiscordHandler(discord_webhook, 'notes-pipeline-alert-agent')
 discord_handler.setFormatter(format)
